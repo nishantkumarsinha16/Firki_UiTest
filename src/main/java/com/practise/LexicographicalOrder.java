068f778d069bfaf5fbfc3f3f600d7383078ff230
nishant <nishantkumarsinha16@gmail.com>
2021-06-05 01:47:20 +0530

    new Code

---
+++ src/main/java/com/practise/LexicographicalOrder.java
@@ -0,0 +1,15 @@
+package com.practise;
+
+public class LexicographicalOrder {
+
+    public static void main(String[] args) {
+
+        String[] word = { "Nishant", "ABCDEF" };
+
+        for (int i = 0; i < word.length; i++) {
+
+        }
+
+    }
+
+}
